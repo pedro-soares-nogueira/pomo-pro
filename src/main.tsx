5dfbd096b08d910920d061dd1391ba897721340b
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-19 11:50:56 -0300

    config:: react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import App from './App'
 import { GlobalStyle } from './styles/global'
@@ -8,8 +9,10 @@ import { defaultTheme } from './styles/themes/default'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
