25cae2949ac43aad8e6de752da7202e90a893353
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-19 11:56:08 -0300

    config:: prettier + eslint

--- src/main.tsx
+++ src/main.tsx
@@ -14,5 +14,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
