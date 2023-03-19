29b8c6c1e90592a00e77dc84d491fb1ae69f7531
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-19 12:14:26 -0300

    styling:: defaul layout

---
+++ src/components/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,27 @@
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink, Outlet } from 'react-router-dom'
+import { HeaderContainer, LayoutContainer } from './styles'
+import logo from '../../../assets/logo.png'
+
+const DefaultLayout = () => {
+  return (
+    <LayoutContainer>
+      <HeaderContainer>
+        <span>
+          <img src={logo} alt="" />
+        </span>
+        <nav>
+          <NavLink to="/" title="Timer">
+            <Timer size={24} />
+          </NavLink>
+          <NavLink to="/history" title="Histórico">
+            <Scroll size={24} />
+          </NavLink>
+        </nav>
+      </HeaderContainer>
+      <Outlet />
+    </LayoutContainer>
+  )
+}
+
+export default DefaultLayout
